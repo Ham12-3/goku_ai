428b0f8b817d228d0e59ab955175cd126265d593
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2024-09-28 14:26:57 +0100

    update

--- client/goku_ai/src/lib/pinecone.ts
+++ client/goku_ai/src/lib/pinecone.ts
@@ -1,4 +1,6 @@
 import { Pinecone } from "@pinecone-database/pinecone";
+import { downloadFromS3 } from "./s3-server";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 
 const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY!,
@@ -7,4 +9,15 @@ const index = pinecone.index("quickstart");
 
 export async function loadS3IntoPinecone(fileKey: string) {
   // obtain the pdf
+
+  console.log("downloading s3 into our file system");
+
+  const file_name = await downloadFromS3(fileKey);
+  if (!file_name) {
+    throw new Error("could not download from s3");
+  }
+
+  const loader = new PDFLoader(file_name);
+  const pages = loader.load();
+  return pages;
 }
--- client/goku_ai/src/lib/s3-server.ts
+++ client/goku_ai/src/lib/s3-server.ts
@@ -1,6 +1,28 @@
 import AWS from "aws-sdk";
+import fs from "fs";
 export async function downloadFromS3(file_key: string) {
   try {
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+    const s3 = new AWS.S3({
+      params: {
+        Bucket: process.env.AWS_BUCKET_NAME,
+      },
+      region: "us-east-1",
+    });
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: file_key,
+    };
+
+    const obj = await s3.getObject(params).promise();
+
+    const file_name = `/tmp/pdf - ${Date.now()}.pdf`;
+
+    fs.writeFileSync(file_name, obj.Body as Buffer);
+    return file_name;
   } catch (error) {
     console.error(error);
     return null;
